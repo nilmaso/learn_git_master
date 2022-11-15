516ecf3ff22c50408776bd0b6515d2c248cc2dca
Nil Masó Castro <nilmasocastro@MacBook-Air-2.local>
2022-11-15 16:59:29 +0100

    Updated the code to analyze the data

--- Data_to_analyze/code_data.py
+++ Data_to_analyze/code_data.py
@@ -45,23 +45,14 @@ data_df = data_df.set_index('regi_policial_rp')
 data_df = data_df.drop('Divisió de Transport')
 data_df = data_df.reset_index()
 
+
 #Since there is 2 RP Metropolitana Nord we need to change the name of these strings
-data_df.loc['RP  Metropolitana Nord', 'regi_policial_rp'] = 'RP Metropolitana Nord'
-
-
-#%%
+data_df = data_df.replace('RP  Metropolitana Nord', 'RP Metropolitana Nord')
+#replace finds all 'RP  Metropolitana Nord' in the dataframe and substitutes with
+#'RP Metropolitana Nord' (only one blankspace between 'RP and Metropolitana')
 
 print(data_df['regi_policial_rp'].value_counts())
-
-
-#%%
-'''
-data_df['mitjana_temps_espera'] = pd.to_datetime(data_df['mitjana_temps_espera'])
-
-data_df['mitjana_temps_espera'] = data_df['mitjana_temps_espera'].datatime.hour * 3600 + \
-                                  data_df['mitjana_temps_espera'].datatime.minute * 60 + \
-                                  data_df['mitjana_temps_espera'].datatime.second
-'''
+#There are more values for RP Metropolitana Nord since there are more ABPs in it
 
 #%%
 #Specific ABP
@@ -80,9 +71,53 @@ mes_visites.plot(kind='bar', title='ABP '+ABP, xlabel='Month', ylabel='Number of
 
 
 
+#%%
+#We want to plot the number of visits in every RP from 2011 to 2021
+
+#We create a DataFrame with 2 columns with the RP and number of visits
+visites_RP = data_df[['regi_policial_rp','nombre_de_visites']].copy()
+
+number_of_years = 10 #to divide and obtain the mean for all years (from 2011 to 2021)
+
+#Write all the RPs in a list
+list_RP = [i for i in (data_df['regi_policial_rp'].value_counts()).index]
+
+
+mean_visites_RP = []
+#%%
+
+(data_df['regi_policial_rp'].value_counts())['RP Ponent']
+
+for RP in list_RP:
+    specific_RP = [i for i in (data_df['regi_policial_rp'].value_counts())[RP]]
+    for row in range(0,len(data_df['regi_policial_rp'])):
+        if data_df.loc[row,'regi_policial_rp']==0:
+        
+
+    visites_RP.append(sum())
 
 
 
+visites_RP = pd.DataFrame(visites_RP)
+
+
+
+#%%
+visites_RP.plot(kind='bar', title='Number of visits for RP', xlabel='RP', ylabel='Average number fo visits', 
+                rot=60, stacked=False, legend=False)
+
+
+
+
+
+#%%
+'''
+data_df['mitjana_temps_espera'] = pd.to_datetime(data_df['mitjana_temps_espera'])
+
+data_df['mitjana_temps_espera'] = data_df['mitjana_temps_espera'].datatime.hour * 3600 + \
+                                  data_df['mitjana_temps_espera'].datatime.minute * 60 + \
+                                  data_df['mitjana_temps_espera'].datatime.second
+'''
 
 #%%
 #Plot with circles of different sizes?
